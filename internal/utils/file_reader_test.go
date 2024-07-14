b2b2979b7afb4032d30052aefac9c67dc177139f
diegoperezeng <diegoperez.eng@gmail.com>
2024-07-13 23:06:13 -0300

    File Reader test adjusted with a real file

--- internal/utils/file_reader_test.go
+++ internal/utils/file_reader_test.go
@@ -1,26 +1,14 @@
 package utils
 
 import (
-	"os"
 	"testing"
 )
 
 func TestReadFile(t *testing.T) {
-	content := "test content"
-	tmpfile, err := os.CreateTemp("", "testfile")
-	if err != nil {
-		t.Fatalf("Unable to create temp file: %v", err)
-	}
-	defer os.Remove(tmpfile.Name())
 
-	if _, err := tmpfile.Write([]byte(content)); err != nil {
-		t.Fatalf("Unable to write to temp file: %v", err)
-	}
-	if err := tmpfile.Close(); err != nil {
-		t.Fatalf("Unable to close temp file: %v", err)
-	}
+	content := "19:30 InitGame:"
 
-	result, err := ReadFile(tmpfile.Name())
+	result, err := ReadFile("../../files/file_for_test.log")
 	if err != nil {
 		t.Fatalf("Error reading file: %v", err)
 	}
