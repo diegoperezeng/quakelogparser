879e4bc0cd9189e3ca92b6362011b055588f151e
diegoperezeng <diegoperez.eng@gmail.com>
2024-07-13 22:35:07 -0300

    First Implementation of file_reader

---
+++ internal/utils/file_reader.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"os"
+)
+
+func ReadFile(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
